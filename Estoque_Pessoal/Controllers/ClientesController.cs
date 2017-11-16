404c524290ebf6441441af5590ab504e7efa3481
Juliana090694 <jujuba_elle@hotmail.com>
2017-11-16 15:09:36 -0200

    Funções de login e logout em Clientes

--- Estoque_Pessoal/Controllers/ClientesController.cs
+++ Estoque_Pessoal/Controllers/ClientesController.cs
@@ -7,6 +7,8 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Estoque_Pessoal.Models;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
 
 namespace Estoque_Pessoal.Controllers
 {
@@ -113,6 +115,52 @@ namespace Estoque_Pessoal.Controllers
             db.ClienteSet.Remove(cliente);
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+
+        // GET: Login
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login (LoginViewModel login)
+        {
+            //Cria o objeto que vai guardar o possível cliente
+            Cliente c = null;
+            //Se foi enviado corretamente
+            if (login != null)
+            {
+                //Se login e senha forem diferente de nulos
+                if (login.Login != "" && login.Senha != "")
+                {
+                    //Procura no banco um login e senha igual e retorna o usuario
+                     c = db.ClienteSet.Where(l => l.Login == login.Login && l.Senha == login.Senha).FirstOrDefault();
+
+                    //Se o cliente for encontrado
+                    if (c != null)
+                    {
+                        //Cria cookie de autenticação
+                        FormsAuthentication.SetAuthCookie(login.Login, false);
+                        //Guarda nome do usuário na session
+                        Session["Nome"] = c.Nome;
+                        return RedirectToAction("Index", "Clientes");
+                    }
+                }
+                //Em caso de algum erro
+                ModelState.AddModelError("", "Usuário ou senha incorretos!");
+                return View();
+            }
+            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        }
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Clientes");
         }
 
         protected override void Dispose(bool disposing)
---
+++ Estoque_Pessoal/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Estoque_Pessoal.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Senha { get; set; }
+
+    }
+}
